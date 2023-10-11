79d46008e9e14db3877112fb2404d90db9566fa8
Max Laptsev <maksru2009@gmail.com>
2023-10-11 23:05:10 +0300

    commit

--- app/src/main/java/testplugin/Master.java
+++ app/src/main/java/testplugin/Master.java
@@ -1,4 +1,15 @@
 package testplugin;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
 public class Master {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        TreeMap<String, String> treeMap = new TreeMap<>();
+
+
+
+    }
 }
